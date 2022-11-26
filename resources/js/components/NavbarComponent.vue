74d1f91384cbd1c88bd7dc1a528df3f57b3381fa
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2022-11-26 21:42:54 +0200

    update

--- resources/js/components/NavbarComponent.vue
+++ resources/js/components/NavbarComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <inertia-link class="navbar-brand" href="/">
+            <a class="navbar-brand" href="/">
                 <img src="/images/logo.png">
-            </inertia-link>
+            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
